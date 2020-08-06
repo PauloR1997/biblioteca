95ff794336aaf50e103b0786af510d11ee2ca585
PauloR1997 <oyepaulo57@gmail.com>
2020-08-06 13:53:10 -0500

    6-atributos-creados

--- src/java/Modelo/cliente.java
+++ src/java/Modelo/cliente.java
@@ -10,12 +10,12 @@ package Modelo;
  * @author Paty Ramirez
  */
 public class cliente {
-    int idcliente;
-    String nombre;
-    String codigo;
-    String ciudad;
-    String tipo;
-    String estado;
+    private int idcliente;
+    private String nombre;
+    private String codigo;
+    private String ciudad;
+    private String tipo;
+    private String estado;
 
     public cliente() {
     }
@@ -27,6 +27,54 @@ public class cliente {
         this.ciudad = ciudad;
         this.tipo = tipo;
         this.estado = estado;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getIdcliente() {
+        return idcliente;
+    }
+
+    public void setIdcliente(int idcliente) {
+        this.idcliente = idcliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
     
 
--- src/java/Modelo/libro.java
+++ src/java/Modelo/libro.java
@@ -9,6 +9,74 @@ package Modelo;
  *
  * @author Paty Ramirez
  */
-public class libro {
-    
+public class libro { 
+    private int idlibro;
+     private String titulo;
+     private String editorial;
+     private String año;
+     private String precio;
+     private String autor;
+
+    public libro() {
+    }
+
+    public libro(int idlibro, String titulo, String editorial, String año, String precio, String autor) {
+        this.idlibro = idlibro;
+        this.titulo = titulo;
+        this.editorial = editorial;
+        this.año = año;
+        this.precio = precio;
+        this.autor = autor;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getIdlibro() {
+        return idlibro;
+    }
+
+    public void setIdlibro(int idlibro) {
+        this.idlibro = idlibro;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getAño() {
+        return año;
+    }
+
+    public void setAño(String año) {
+        this.año = año;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+     
 }
+    
+
